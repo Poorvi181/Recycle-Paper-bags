595ce35a35f6d1201cd41c82cf447a556422d693
Poorvi181 <mannarchana@yahoo.co.uk>
2024-11-10 15:06:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,20 +10,47 @@ Gameover=False
 Gamecomplete=False
 Currentlevel=1
 items=["bag","battery","bottle","chips"]
+players=[]
 def draw():
     screen.blit("bg2",(0,0))
+    for player in players:
+        player.draw()
+
 def update():
-    pass
+    global players,Currentlevel
+    if len(players)==0:
+        players=creation(Currentlevel)
+    
+
+def creation(numberofitems):
+    itemstocreate=makeitems(numberofitems)
+    newitems=createitems(itemstocreate)
+    layoutitems(newitems)
+    return newitems()
 
 def makeitems(numberofitems):
-    numberofitems=["paperimg"]
+    numberofitem=["paperimg"]
     for i in range(0,numberofitems):
         randomitems=random.choice(items)
-        numberofitems.append(randomitems)
-    return numberofitems
+        numberofitem.append(randomitems)
+    return numberofitem
+
+
+def createitems(numberofitems):
+    newitems=[] 
+    for i in range(0,numberofitems): 
+        item=Actor(i)
+        newitems.append(item)
+    return newitems
+
+def layoutitems(numberofitems):
+    numberofgaps=len(numberofitems)+1
+    gapsize=WIDTH/numberofgaps
+    random.shuffle(numberofitems)
+    for index,item in enumerate(numberofitems):
+        newXpos=(index+1)*gapsize
+        item.x=newXpos
 
 
 
-        
 pgzrun.go()
-
